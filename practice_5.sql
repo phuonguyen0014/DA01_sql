3aadc0caa12093738f16c463eca5bf58ae2a650d
phuonguyen0014 <122925013+phuonguyen0014@users.noreply.github.com>
2023-10-26 01:33:51 -0400

    Update practice_5.sql

--- practice_5.sql
+++ practice_5.sql
@@ -1,13 +1,20 @@
 --baitap1
 
-select COUNTRY.name, CEILING(AVG(CITY.population))
+select COUNTRY.name, CEILING(AVG(CITY.population))  -- dùng FLOOR - rounded down to the nearest integer - làm tròn ở cận dưới
+from COUNTRY
+join CITY 
+on CITY.countrycode = COUNTRY.code
+group by COUNTRY.name;
+
+--> Sửa lại
+select COUNTRY.name, FLOOR(AVG(CITY.population)) as  average_population
 from COUNTRY
 join CITY 
 on CITY.countrycode = COUNTRY.code
 group by COUNTRY.name;
 
 --baitap2
-SELECT ROUND(
+SELECT ROUND(		-- round(a/b) =>> ta phải cast a hoặc b về decimal --> CAST() function converts a value (of any type) into a specified datatype.
   SUM(CASE
 WHEN texts.signup_action = 'Confirmed' THEN 1 
 ELSE 0 
@@ -16,43 +23,57 @@ FROM emails
 LEFT JOIN texts
 ON emails.email_id = texts.email_id;
 
+--> Sửa lại
+SELECT
+ROUND(cast(
+  SUM(CASE
+WHEN texts.signup_action = 'Confirmed' THEN 1 
+ELSE 0 
+END) as decimal)/ COUNT(*), 2)
+FROM emails
+LEFT JOIN texts
+ON emails.email_id = texts.email_id;
+
+
 --baitap3
-SELECT b.age_bucket, 
-ROUND(
-(
-  (CASE WHEN a.activity_type = 'send' then a.time_spent ELSE 0 END) /
-  ((CASE WHEN a.activity_type = 'send' then a.time_spent ELSE 0 END) +
-    (CASE WHEN a.activity_type = 'open' then a.time_spent ELSE 0 END)
-  )
-) * 100.0, 2) as send_perc,
-ROUND(
-(
-  (CASE WHEN a.activity_type = 'open' then a.time_spent ELSE 0 END) /
-  ((CASE WHEN a.activity_type = 'send' then a.time_spent ELSE 0 END) +
-    (CASE WHEN a.activity_type = 'open' then a.time_spent ELSE 0 END)
-  )
-) * 100.0, 2) as open_perc
+SELECT 
+    b.age_bucket, 
+    ROUND(
+        SUM(CASE WHEN a.activity_type = 'send' THEN a.time_spent ELSE 0 END) / 
+        (SUM(CASE WHEN a.activity_type = 'send' THEN a.time_spent ELSE 0 END) + 
+         SUM(CASE WHEN a.activity_type = 'open' THEN a.time_spent ELSE 0 END)
+        ) * 100.0, 2
+    ) AS send_perc,
+    ROUND(
+        SUM(CASE WHEN a.activity_type = 'open' THEN a.time_spent ELSE 0 END) / 
+        (SUM(CASE WHEN a.activity_type = 'send' THEN a.time_spent ELSE 0 END) + 
+         SUM(CASE WHEN a.activity_type = 'open' THEN a.time_spent ELSE 0 END)
+        ) * 100.0, 2
+    ) AS open_perc
 FROM activities a
-INNER JOIN age_breakdown b
-ON a.user_id = b.user_id
+INNER JOIN age_breakdown b ON a.user_id = b.user_id
 GROUP BY b.age_bucket;
---column "a.activity_type" must appear in the GROUP BY clause or be used in an aggregate function (LINE: 3)
--- tai sao em phai dung GROUP BY o cau nay?
 
 --baitap4
-SELECT c.customer_id
-FROM customer_contracts c
-LEFT JOIN products p
-ON c.product_id = p.product_id
-GROUP BY c.customer_id
-HAVING COUNT(DISTINCT p.product_category) = (SELECT COUNT(DISTINCT product_category) FROM products);
+SELECT cc.customer_id
+FROM customer_contracts cc
+JOIN products p 
+ON cc.product_id = p.product_id
+GROUP BY cc.customer_id
+HAVING 
+    SUM(CASE WHEN p.product_category = 'Analytics' THEN 1 ELSE 0 END) >= 1
+    AND SUM(CASE WHEN p.product_category = 'Containers' THEN 1 ELSE 0 END) >= 1
+    AND SUM(CASE WHEN p.product_category = 'Compute' THEN 1 ELSE 0 END) >= 1;
+
 
 --baitap5
-select f1.employee_id, f1.name, count(f1.employee_id) as reports_count, round(avg(f2.age),0) as average_age 
+select f1.employee_id, f1.name, 
+	count(f2.employee_id) as reports_count, 
+	round(avg(f2.age),0) as average_age 
 from Employees f1
 join Employees f2
-on f1.reports_to = f2.reports_to
-group by f1.employee_id
+on f1.employee_id = f2.reports_to
+group by f1.name
 HAVING COUNT(DISTINCT f1.employee_id) >= 1;
 
 --baitap6
