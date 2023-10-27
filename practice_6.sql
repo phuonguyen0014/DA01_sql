3778f87ecf3dfd7f8674d275a6a224cc885995c3
phuonguyen0014 <122925013+phuonguyen0014@users.noreply.github.com>
2023-10-27 02:26:39 -0400

    Update practice_6.sql

--- practice_6.sql
+++ practice_6.sql
@@ -1,9 +1,29 @@
+--baitap2
+select category, 
+  product, 
+  SUM(spend) as total_spend
+from product_spend
+where EXTRACT(year from transaction_date) = 2022
+      and spend IN (
+                    select MAX(spend) 
+                    from product_spend
+                    where EXTRACT(year from transaction_date) = 2022
+                    GROUP BY product
+                    ORDER BY MAX(spend) ASC
+                    limit 2) 
+group by category, product;
+-- bài này em bị rối quá, em ko bị sai khúc nào rồi ạ
+
+--baitap3
+select COUNT(policy_holder_id) as member_count
+from callers
+GROUP BY policy_holder_id
+HAVING count(policy_holder_id) >= 3;
 
 --baitap4
 /*
 Write a query to return the IDs of the Facebook pages that have zero likes. The output should be sorted in ascending order based on the page IDs.
 */
-
 select page_id 
 from pages
 where page_id NOT IN (select page_id from page_likes)
@@ -47,6 +67,8 @@ SELECT
    join num_approved_aapproved_trans b
    on b.id = a.id;
 
+--cho em hỏi là có cách nào extract được cả year và month cùng lúc được ko hay bắt buộc phải extract từng cái một rồi nối lại với nhau
+
 --baitap7
 select p.product_id, MIN(year) as first_year, s.quantity, s.price
 from product p
@@ -80,3 +102,43 @@ where (title, description) IN
     HAVING COUNT(*) > 1);
 -- Subquery -> trả về các giá trị title, description bị trùng lặp
 
+--baitap11
+WITH user_rating AS (
+select u.user_id, u.name, count(mr.movie_id) as user_rate
+from users u
+left join movierating mr
+on u.user_id = mr.user_id
+group by u.user_id)
+,
+movie_rating AS (
+select m.movie_id, m.title, avg(mr.rating) as avg_rate
+from movies m
+left join movierating mr
+on m.movie_id = mr.movie_id
+where created_at >= '2020-02-01' and created_at < '2020-03-01'
+group by mr.movie_id)
+
+--retrieve name of the user who has rated the greatest number of movies. In case of a tie, return the lexicographically smaller user name
+select name
+from user_rating
+where user_rate = (select MAX(user_rate) from user_rating)
+order by name ASC
+limit 1;
+
+--retrieve movie name with the highest average rating in February 2020. In case of a tie, return the lexicographically smaller movie name.
+select title
+from avg_rate
+where avg_rate = (select MAX(avg_rate) from avg_rate)
+order by title ASC
+limit 1;
+
+--> em ko biết kết hợp lại với nhau như thế nào :((
+
+--baitap12
+select a.accepter_id as id, count(*) as num
+from RequestAccepted r
+left join RequestAccepted a
+on r.accepter_id = a.requester_id
+where .....
+
+--câu này em ko biết nên làm gì tiếp theo :(((
